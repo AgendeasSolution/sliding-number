594e0df5b84434a9fdb402e5df9be357ca153259
Shyam Mendapara <shyampmendapara@gmail.com>
2025-11-15 22:53:43 +0530

    Add Google services Gradle plugin and Firebase BoM dependency
    Included the Google services Gradle plugin in the project configuration to enable Firebase integration. Added the Firebase BoM dependency to manage Firebase library versions consistently, and prepared for future Firebase product integrations.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     id("com.android.application")
     id("kotlin-android")
+    // Add the Google services Gradle plugin
+    id("com.google.gms.google-services")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
 }
@@ -44,5 +46,15 @@ flutter {
 }
 
 dependencies {
+    // Import the Firebase BoM
+    implementation(platform("com.google.firebase:firebase-bom:34.5.0"))
+
+    // TODO: Add the dependencies for Firebase products you want to use
+    // When using the BoM, don't specify versions in Firebase dependencies
+    implementation("com.google.firebase:firebase-analytics")
+
+    // Add the dependencies for any other desired Firebase products
+    // https://firebase.google.com/docs/android/setup#available-libraries
+
     implementation("com.facebook.android:facebook-android-sdk:[8,9)")
 }
